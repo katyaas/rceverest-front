28dab0a71519ac137866dae8d6de830313487290
Strekalova Ekaterina <ekaterina.strekalova@vedidev.com>
2019-05-17 12:06:07 +0700

    add meta tag

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,7 @@ export default class MyDocument extends Document {
       <html>
       <Head>
         <link rel="stylesheet" href="/_next/static/style.css" />
+        <meta name="google-site-verification" content="VT8C2BQhiGWT0blymCBgforOARaqybQmKvobGrRpuMg" />
       </Head>
       <body style={{
         backgroundImage: 'linear-gradient(rgba(255,255,255,0.2), rgba(255,255,255,0.2)), url(/static/background2.jpg)',
